8fa338ca0a702024277d6150a508aa629bc180f3
ajaidanial <ajaidanial@gmail.com>
2020-03-03 23:29:21 +0530

    all done

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import copy
 app = Flask(__name__)
 
 mydb = mysql.connector.connect(
-    host="localhost", database="formdata1", user="root", password="pr09r4mm3r"
+    host="localhost", database="formdata4", user="root", password="pr09r4mm3r"
 )
 mycursor = mydb.cursor(buffered=True)
 
@@ -51,49 +51,12 @@ def form1():
     return render_template("form1.html", form=form, records=records)
 
 
-@app.route("/form2", methods=["GET", "POST"])
-def form2():
-    form = forms.Form2Form()
-
-    # if form is submitted
-
-    if form.validate_on_submit():
-        # breakpoint()
-        timestamp = datetime.now()
-        # deepcopy the data to remove csrf_token
-        data = copy.deepcopy(form.data)
-        data.pop("csrf_token")
-
-        # for creating the new columns
-        for name in data.keys():
-
-            try:  # check if the column_name is already in table
-                mycursor.execute(f"SELECT {name} from `form2`;")
-                mycursor.fetchone()
-            except:  # if error | no column in table | create it | NOTE: varchar(15) => reduced the size for memory overflow
-                mycursor.execute(f"ALTER TABLE `form2` add {name} varchar(15);")
-
-        # insert into the table
-        names = data.keys()
-        values = data.values()
-
-        mycursor.execute(
-            f"INSERT INTO form2 ({', '.join(names)}) VALUES {tuple(values)};"
-        )
-        mydb.commit()
-
-        # success message
-        return "success"
-    # breakpoint()
-    # for GET request
-    return render_template("form2.html", form=form)
-
-
 @app.route("/form3", methods=["GET", "POST"])
 def form3():
     form = forms.Form3Form()
     # if form is submitted
     if form.validate_on_submit():
+
         timestamp = datetime.now()
         # deepcopy the data to remove csrf_token
         data = copy.deepcopy(form.data)
@@ -101,19 +64,18 @@ def form3():
 
         # for creating the new columns
         for name in data.keys():
-
             try:  # check if the column_name is already in table
-                mycursor.execute(f"SELECT {name} from `form3`;")
+                mycursor.execute(f"SELECT {name} from `form4`;")
                 mycursor.fetchone()
             except:  # if error | no column in table | create it | NOTE: varchar(15) => reduced the size for memory overflow
-                mycursor.execute(f"ALTER TABLE `form3` add {name} varchar(15);")
+                mycursor.execute(f"ALTER TABLE `form4` add {name} varchar(15);")
 
         # insert into the table
         names = data.keys()
         values = data.values()
 
         mycursor.execute(
-            f"INSERT INTO form3 ({', '.join(names)}) VALUES {tuple(values)};"
+            f"INSERT INTO form4 ({', '.join(names)}) VALUES {tuple(values)};"
         )
         mydb.commit()
 
